8aedd00fe097a8cc1dbd8616cb9d4cb6401d708e
rikellme <rikellmealencar02@gmail.com>
2026-02-22 22:36:44 -0400

    feat add

--- src/services-server/api.ts
+++ src/services-server/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const isProd = process.env.NODE_ENV === "production";
 const API_URL = isProd
   ? process.env.API_URL_PROD ?? process.env.API_URL
-  : process.env.API_URL ?? "http://localhost:3030";
+  : process.env.API_URL ?? "https://roll-zen-keep-win.trycloudflare.com";
 
 export const backendApi = axios.create({
   baseURL: API_URL,
